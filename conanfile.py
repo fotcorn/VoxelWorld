f38041d54067c36dbb2b8fb00bce4a83a7776845
fotcorn <fotcorn@gmail.com>
2020-01-22 14:32:30 +0000

    use conanfile.py to make use of conan profiles easier

---
+++ conanfile.py
@@ -0,0 +1,20 @@
+from conans import ConanFile, CMake
+
+class VoxelWorld(ConanFile):
+  settings = 'os', 'compiler', 'build_type', 'arch'
+  requires = [
+    'glew/2.1.0@bincrafters/stable',
+    'glfw/3.2.1@bincrafters/stable',
+    'glm/0.9.9.1@g-truc/stable',
+    'stb/20180214@conan/stable',
+    'imgui/1.62@bincrafters/stable',
+    'fmt/5.2.1@bincrafters/stable',
+    'Assimp/4.1.0@jacmoe/stable',
+    'boost/1.70.0@conan/stable',
+  ]
+  generators = 'cmake',
+
+  def build(self):
+    cmake = CMake(self)
+    cmake.configure()
+    cmake.build()
