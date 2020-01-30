0a00a5682dac6a025890e21e4b002a3a35226443
fotcorn <fotcorn@gmail.com>
2020-01-30 10:12:27 +0000

    emscripten: disable memory growth

--- emscripten.sh
+++ emscripten.sh
@@ -6,4 +6,4 @@ source activate.sh
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j16
 mv bin/VoxelWorld VoxelWorld.bc
-emcc VoxelWorld.bc -o VoxelWorld.html -s USE_GLFW=3 -s USE_WEBGL2=1 --preload-file VoxelWorld/resources -s DISABLE_EXCEPTION_CATCHING=0 -s EXCEPTION_DEBUG=1 -s ALLOW_MEMORY_GROWTH=1
+emcc VoxelWorld.bc -o VoxelWorld.html -s USE_GLFW=3 -s USE_WEBGL2=1 --preload-file VoxelWorld/resources -s DISABLE_EXCEPTION_CATCHING=0 -s EXCEPTION_DEBUG=1
