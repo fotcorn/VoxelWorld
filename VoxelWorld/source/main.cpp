9e228540c1865df6d591a5afc964d8e02544ed85
fotcorn <fotcorn@gmail.com>
2020-01-31 13:36:02 +0000

    emscripten: enhance error reporting

--- VoxelWorld/source/main.cpp
+++ VoxelWorld/source/main.cpp
@@ -1,22 +1,31 @@
 #include "RenderLoop.h"
 
+#ifdef EMSCRIPTEN
+
+#include <emscripten.h>
 #include <iostream>
 
-#ifdef EMSCRIPTEN
-#include <emscripten.h>
-#endif
+std::shared_ptr<RenderLoop> loop;
 
 void update() {
-    static RenderLoop loop;
-    loop.mainLoop();
+    loop->mainLoop();
 }
 
 int main() {
-#ifdef EMSCRIPTEN
+    try {
+        loop = std::make_shared<RenderLoop>();
+    } catch (std::runtime_error& e) {
+        std::cout << e.what() << std::endl;
+    }
     emscripten_set_main_loop(update, 0, 1);
-#else
-    RenderLoop loop;
-    loop.mainLoop();
-#endif
     return 0;
 }
+#else
+
+int main() {
+    RenderLoop loop;
+    loop.mainLoop();
+    return 0;
+}
+
+#endif
