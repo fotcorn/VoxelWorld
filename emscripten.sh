56c77a4f9b82bb4ad395d0826fa9af4b391802fb
fotcorn <fotcorn@gmail.com>
2020-01-31 13:33:06 +0000

    emscripten: fix build script

--- emscripten.sh
+++ emscripten.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -exo pipefail
 #mkdir build-emscripten
 #cd build-emscripten
 #conan install .. --build=missing -pr=../emscripten.profile
@@ -7,4 +7,4 @@ source activate.sh
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j16
 mv bin/VoxelWorld VoxelWorld.bc
-emcc VoxelWorld.bc -o VoxelWorld.html -s USE_GLFW=3 -s USE_WEBGL2=1 --preload-file VoxelWorld/resources --shell-file ../emscripten_shell.html
+emcc VoxelWorld.bc -o VoxelWorld.html -s USE_GLFW=3 -s USE_WEBGL2=1 -s DISABLE_EXCEPTION_CATCHING=0 -s EXCEPTION_DEBUG=1 --preload-file VoxelWorld/resources --shell-file ../emscripten_shell.html
